d7a95c1597dae84e4f8934077973f41e87a46865
eminem <ghy1060627080@163.com>
2020-12-31 18:48:15 +0800

    项目初始化

--- web-music/src/main.js
+++ web-music/src/main.js
@@ -3,8 +3,15 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+//初始样式重置
+import './assets/styles/reset.css'
+import './assets/styles/border.css'
+import fastClick from 'fastclick'
+
 
 Vue.config.productionTip = false
+// 使用 astClick移动端300毫秒点击延时问题
+fastClick.attach(document.body)
 
 /* eslint-disable no-new */
 new Vue({
