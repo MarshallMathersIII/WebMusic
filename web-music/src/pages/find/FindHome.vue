a8702c3b21a60ca73284b26f694cfb7166852408
eminem <ghy1060627080@163.com>
2021-01-14 14:27:08 +0800

    fix（发现模块）：推荐歌单宽高比防止页面坍塌

--- web-music/src/pages/find/FindHome.vue
+++ web-music/src/pages/find/FindHome.vue
@@ -17,10 +17,12 @@
       <div>推荐歌单</div>
       <div class="more">更多</div>
     </div>
-    <div class="song-list">
-      <div class="song-item" v-for="item in playList">
-        <img v-lazy="item.coverImgUrl" alt class="song-img" />
-        <span>{{item.name}}</span>
+    <div class="wapper">
+      <div class="song-list">
+        <div class="song-item" v-for="item in playList">
+          <img :src="item.coverImgUrl" alt class="song-img" />
+          <span>{{item.name}}</span>
+        </div>
       </div>
     </div>
     <!-- 分割线 -->
@@ -202,30 +204,34 @@ export default {
     justify-content space-between
     align-items center
     margin 14px 10px 0 10px
-  .song-list
-    display flex
-    justify-content center
-    align-items center
-    justify-content space-between
-    overflow-x auto
-    overflow-y hidden
-    .song-item
-      height 120px
-      padding 10px
-      white-space nowrap
+  .wapper
+    overflow: hidden;
+    height: 0px;
+    padding-bottom: 39%;
+    .song-list
       display flex
-      flex-direction column
       justify-content center
-      align-items flex-start
-      .song-img
-        border-radius 10px
-        height 100px
-        width 100px
-      span
-        margin-top 4px
-        width 100px
-        font-size 8px
-        no-wrap()
+      align-items center
+      justify-content space-between
+      overflow-x auto
+      overflow-y hidden
+      .song-item
+        height 120px
+        padding 10px
+        white-space nowrap
+        display flex
+        flex-direction column
+        justify-content center
+        align-items flex-start
+        .song-img
+          border-radius 10px
+          height 100px
+          width 100px
+        span
+          margin-top 4px
+          width 100px
+          font-size 8px
+          no-wrap()
   .divider
     divider()
   .new-title
