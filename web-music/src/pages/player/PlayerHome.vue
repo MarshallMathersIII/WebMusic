f5b4ce7c5d84e32c9f156ee8715b3ea8f6a52a21
eminem <ghy1060627080@163.com>
2021-01-25 19:35:49 +0800

    feat（播放模块）：页面优化，唱针唱盘动画，iconfont更新

--- web-music/src/pages/player/PlayerHome.vue
+++ web-music/src/pages/player/PlayerHome.vue
@@ -3,23 +3,24 @@
     <title-bar :milddleTip="milddleTip" v-on:leftBtn="leftClickBtn"></title-bar>
     <div class="vinyl-content">
       <div class="stylus">
-        <img src="../../assets/img/stylus.png" alt />
+        <div class="spot"></div>
+        <img src="../../assets/img/stylus.png" :class="[isPlaying?playClass:stopClass]" />
       </div>
-      <div class="vinyl"></div>
+      <div :class="[isPlaying?vinyl:vinylStop]">gil scott-heron</div>
     </div>
     <div class="btn-wapper">
-      <i class="iconfont icon-pengyou"></i>
-      <i class="iconfont icon-pengyou"></i>
-      <i class="iconfont icon-pengyou"></i>
-      <i class="iconfont icon-pengyou"></i>
+      <i class="iconfont icon-xihuan1"></i>
+      <i class="iconfont icon-xiazai1"></i>
+      <i class="iconfont icon-pinglun1"></i>
+      <i class="iconfont icon-gengduo1"></i>
     </div>
     <div class="palyer-progress"></div>
     <div class="palyer">
-      <i class="iconfont icon-pengyou"></i>
-      <i class="iconfont icon-pengyou"></i>
-      <i class="iconfont icon-pengyou"></i>
-      <i class="iconfont icon-pengyou"></i>
-      <i class="iconfont icon-pengyou"></i>
+      <i class="iconfont icon-suijibofang"></i>
+      <i class="iconfont icon-shangyishouxianxing"></i>
+      <i class="iconfont icon-bofang" @click="play"></i>
+      <i class="iconfont icon-xiayishouxianxing"></i>
+      <i class="iconfont icon-juxing"></i>
     </div>
   </div>
 </template>
@@ -37,12 +38,20 @@ export default {
     return {
       milddleTip: "播放组件",
       phone: "",
-      btnShow: false
+      btnShow: false,
+      isPlaying: true,
+      playClass: "play",
+      stopClass: "stop",
+      vinyl: "vinyl",
+      vinylStop: "vinyl-stop"
     };
   },
   methods: {
     leftClickBtn() {
       this.$router.back(-1);
+    },
+    play() {
+      this.isPlaying = this.isPlaying ? false : true;
     }
   }
 };
@@ -64,11 +73,35 @@ export default {
     justify-content flex-start
     flex-direction column
   .stylus
+    display flex
+    align-items center
+    justify-content flex-start
+    flex-direction column
     z-index 1000
-    img
+    .spot
+      position relative
+      top 15px
+      z-index 2000
+      width 30px
+      height 30px
+      border-radius 50%
+      background-color pink
+    .stop
       margin-left 70px
       height 140px
       width 100px
+      transform translate(20px, 0)
+      transform rotate(-15deg)
+      transform-origin top left // 原点设置
+      transition transform 0.2s
+    .play
+      margin-left 70px
+      height 140px
+      width 100px
+      transform translate(20px, 0)
+      transform rotate(0deg)
+      transform-origin top left // 原点设置
+      transition transform 0.2s
   .vinyl
     position relative
     bottom 60px
@@ -76,24 +109,52 @@ export default {
     width 300px
     background url('../../assets/img/vinyl.png')
     background-size 300px 300px
+    display flex
+    align-items center
+    justify-content center
+    animation palying 4s infinite linear
+    animation-fill-mode forwards
+    -webkit-animation-iteration-count 1
+    -webkit-animation-fill-mode forwards
+  @keyframes palying
+    0%
+      -webkit-transform rotate(0deg)
+      transform rotate(0deg)
+    100%
+      -webkit-transform rotate(360deg)
+      transform rotate(360deg)
+  .vinyl-stop
+    position relative
+    bottom 60px
+    height 300px
+    width 300px
+    background url('../../assets/img/vinyl.png')
+    background-size 300px 300px
+    display flex
+    align-items center
+    justify-content center
 .btn-wapper
+  height 40px
   display flex
   justify-content space-around
   align-items center
   i
-    font-size 30px
-    color $color-theme
+    font-size 24px
+    color black
 .palyer-progress
-  margin-top 20px
-  height 40px
   background-color pink
+  height 30px
 .palyer
+  position fixed
+  bottom 0
+  width 100%
   display flex
   justify-content space-around
   align-items center
+  padding 20px 0px
   i
     margin-top 16px
-    font-size 30px
+    font-size 28px
     color $color-theme
   :nth-child(3)
     font-size 40px
